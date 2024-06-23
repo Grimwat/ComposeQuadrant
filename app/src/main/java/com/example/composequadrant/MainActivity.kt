f1ce0a9a661718fae1dd4798444edf3b6148d727
Thomas <Thomas.watson555@Gmail.com>
2024-06-23 20:57:27 +1200

    2nd attempt

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,22 +3,25 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,79 +33,105 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Quadrant ()
+                    Quadrant()
                 }
             }
         }
     }
 }
+
 @Composable
-fun Topleft (head2: String, para2: String, modifier: Modifier = Modifier ) {
+fun Texttr(head1: String,para1: String, modifier: Modifier)
+{
 
-    Text(
-        text = head2,
-        fontWeight = FontWeight.Bold,
-        Modifier = Modifier
-            .padding(16.dp)
-    )
-    Text (
-        text = para2,
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier) {
 
-    )
+        Text(
+            text = head1,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(bottom = 16.dp)
+            )
+        Text(
+            text = para1)
+    }
 }
-
 @Composable
-fun Topright (head1: String, para1: String, modifier: Modifier = Modifier) {
+fun Texttl(head2: String,para2: String, modifier: Modifier)
+{
 
-    Image(
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier) {
 
-    )
-    Text(
-        text = head1,
-        fontweight = FontWeight.Bold,
-        textAlign = TextAlign.Center,
-        Modifier = Modifier
-            .padding(16.dp)
-    )
-    Text(
-        text = para1,
-        textAlign = TextAlign.Center,
-        Modifier = modifier
-
-    )
-}
-
-@Composable
-fun Quadrant(head1: String, para1: String, modifier: Modifier = Modifier){
-    Column(){
-        Row{
-            Box{
-
-            }
-            Box{
-
-            }
-        }
-        Row{
-            Box{
-
-            }
-            Box{
-
-            }
-        }
-
+        Text(
+            text = head2,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = para2)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeQuadrantTheme {
-        Topright(
-        head1 = stringResource(R.string.Head1),
-        para1 = stringResource(R.string.para1)
-        )
+fun Quadrant( modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
+            Box{
+
+                Texttl(
+                    head2 = stringResource(R.string.head2),
+                    para2 = stringResource(R.string.para2),
+                    modifier = modifier
+                )
+
+            }
+            Box{
+                Texttr(
+                    head1 = stringResource(R.string.Head1),
+                    para1 = stringResource(R.string.para1),
+                    modifier = modifier
+                )
+
+            }
+        }
+        Row{
+            Box{
+
+            }
+            Box{
+
+            }
+        }
 
     }
 }
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeQuadrantTheme {
+        Texttr(
+            modifier = Modifier,
+            head1 = stringResource(R.string.Head1),
+            para1 = stringResource(R.string.para1),
+        )
+        Texttl(
+            modifier = Modifier,
+            head2 = stringResource(R.string.head2),
+            para2 = stringResource(R.string.para2),
+        )
+    }
+}
+        
